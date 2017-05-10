599a1a2efa8f04f8f2337d284e3c31f7a2fc4ed8
Benjamin Du <duchuanlong+git@gmail.com>
2017-05-10 23:22:26 +0800

    do not create directories

--- init.sh
+++ init.sh
@@ -6,10 +6,8 @@ PASSWORD=${DOCKER_PASSWORD:-$USER}
 useradd -o -u $USER_ID -d /home/$USER -s /bin/bash -c "$USER" $USER 
 gpasswd -a $USER sudo 
 export HOME=/home/$USER
-mkdir -p $HOME
-chown -R $USER:$USER /home/$USER 
-mkdir -p /jupyter
-chown -R $USER:$USER /jupyter
+# mkdir -p $HOME
+# chown -R $USER:$USER /home/$USER 
 echo ${USER}:${PASSWORD} | chpasswd
 
 su -m $USER && echo $PASSWORD | sudo -S -u $USER ${1:-/script.sh}
